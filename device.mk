9f143906c6ca5c9508340b9068b69d8aa657ac79
danascape <danascape@gmail.com>
2023-11-12 10:29:24 +0100

    ingot: Drop atrace HAL
    It is deprecated in Android 14.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- device.mk
+++ device.mk
@@ -102,10 +102,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
