b918da4962b916a8776cb6bf42c9d16486438bf3
pppig236 <priv@pppig236.com>
2024-10-28 22:47:10 -0400

    ingot: Import soong namespace from hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders
    Change-Id: Id6a29b00286c7de8bf25796af9b6228c18b3ad9a
    Signed-off-by: pppig236 <priv@pppig236.com>

--- device.mk
+++ device.mk
@@ -316,7 +316,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders
 
 # Telephony
 PRODUCT_PACKAGES += \
