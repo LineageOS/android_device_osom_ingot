9bbae77074e16cab00f76ad5aa94115268ce4e7a
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    ingot: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -324,7 +324,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
 
 # Telephony
 PRODUCT_PACKAGES += \
