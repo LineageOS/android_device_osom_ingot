87394809ecd0a4d9aa8a8ff253ef837e3761ae79
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 12:21:55 +0200

    ingot: Patch qccsyshal to load versioned libprotobuf
    Change-Id: I9ec95bf55afd62a03b2bd787c8b82c15b921a452

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/bin/qcc-trd': blob_fixup()
         .replace_needed('libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'),
     'vendor/etc/libnfc-hal-st.conf': blob_fixup()
