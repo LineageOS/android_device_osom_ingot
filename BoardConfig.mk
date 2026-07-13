db6d9292d3113184448b637378c064882f4d1555
Alexander Koskovich <akoskovich@pm.me>
2026-07-13 17:45:37 -0400

    ingot: Drop qcom_dload_mode.download_mode=0 from cmdline
    This is required for working pstore, otherwise the device never enters
    crashdump on reset. OSOM's firmware replaces the stock crashdump flow
    with a pstore dumper. Without entering crashdump, we never get pstore.
    
    Change-Id: I0795a2a50408856c6bcd335e00672d6e08bd3c2d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,6 @@ TARGET_NO_BOOTLOADER := true
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE += qcom_dload_mode.download_mode=0
 BOARD_BOOTCONFIG += androidboot.hardware=qcom
 BOARD_BOOTCONFIG += androidboot.memcg=1
 BOARD_BOOTCONFIG += androidboot.usbcontroller=a600000.dwc3
